a98d228648cfcb9d38b8a20e5bf15a6a43756e78
Marisol Rodriguez <rodriguezsmarisol@gmail.com>
2022-07-23 17:59:19 -0300

    Desafio 1: Agrego Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,12 +2,16 @@ import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <nav className='Navbar' >
-        <ul id="menu">
-          <li><a href="#tienda">Tienda</a></li>
-          <li><a href="#contacto">Contacto</a></li>
-        </ul>
-       </nav>
+        <header>
+         <nav className='Navbar' >
+            <h1>Romilaria Flama</h1>
+            <div className='Menu'>
+              <button>Ver todo</button>
+              <button>Dijes</button>
+              <button>Aros</button>
+            </div>
+         </nav>
+       </header>
     )
 }
 

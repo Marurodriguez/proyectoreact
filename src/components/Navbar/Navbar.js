7207965afda84fa679dffc2c650056f497e208df
Marisol Rodriguez <rodriguezsmarisol@gmail.com>
2022-07-23 16:13:43 -0300

    node_modules

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav className='Navbar' >
+        <ul id="menu">
+          <li><a href="#tienda">Tienda</a></li>
+          <li><a href="#contacto">Contacto</a></li>
+        </ul>
+       </nav>
+    )
+}
+
+export default Navbar;

36b09fc032e5811b4e6f0a38405b1e023ee87a44
Marisol Rodriguez <rodriguezsmarisol@gmail.com>
2022-07-23 18:16:09 -0300

    Algunos retoques

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,2 @@
+import '.CartWidget.css'
+
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,11 @@
 import './Navbar.css'
 
+
 const Navbar = () => {
     return (
         <header>
          <nav className='Navbar' >
+            <img className='logo' src='images/logo.png' alt='Logo'></img>
             <h1>Romilaria Flama</h1>
             <div className='Menu'>
               <button>Ver todo</button>
